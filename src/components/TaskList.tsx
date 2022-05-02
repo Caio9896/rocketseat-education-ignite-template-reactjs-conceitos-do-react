93d4ceb44f216b2a8edfc22fa369d977f42656c8
Caio Martins <caio.martins176@gmail.com>
2022-05-02 12:31:43 -0300

    Desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import '../styles/tasklist.scss'
-
+import _ from 'lodash'
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 import { api } from '../services/api';
 
@@ -15,26 +15,30 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
-  
+
+  /* Usar com miragejs
   useEffect(() => {
     api.get('/tasks').then(response => setTasks(response.data.tasks))
   }, []);
+  */
 
-  async function handleCreateNewTask() {
-     //Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if(newTaskTitle === ''){
-      alert('Preencha o campo "Adicionar Novo Todo"')
+  function handleCreateNewTask() {
+    //Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    //para conexao com o miragejs
+    /* const response = await api.post('/tasks', {
+      title: newTaskTitle,
+      isComplete: false
+    })*/
+
+    if (!newTaskTitle) {
+      return
     } else {
-      const response = await api.post('/tasks', {
-        title: newTaskTitle,
-        isComplete: false
-      })
 
       setTasks([
         ...tasks,
         {
-          id: response.data.task.id,
-          title: response.data.task.title,
+          id: Math.random(),
+          title: newTaskTitle,
           isComplete: false
         }
       ])
@@ -42,22 +46,37 @@ export function TaskList() {
     setNewTaskTitle('');
   }
 
-  async function handleToggleTaskCompletion(e: boolean, key: number) {
+  function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    /*para conexao com o miragejs
     const response = await api.patch('/tasks', {
         id: key,
         isComplete: e
-    })
-    setTasks(response.data.tasks)
+    })*/
+
+    //se 'task === id' for igual a true retorna o objeto, senao retorno o proprio objeto
+    const updatedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task)
+
+    setTasks(updatedTask)
+
   }
 
-  async function handleRemoveTask(key: number) {
+  function handleRemoveTask(key: number) {
+    //para conexao com o miragejs
+    /*
     const response = await api.delete('/tasks', {
       data: {
         id: key
       }
+    })*/
+
+    const searchTasks = _.remove(tasks, (task) => {
+      return task.id != key
     })
-    setTasks(response.data.tasks)
+    setTasks(searchTasks)
   }
 
   return (
@@ -66,15 +85,15 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             value={newTaskTitle}
             onChange={(e) => setNewTaskTitle(e.target.value)}
           />
 
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
@@ -85,11 +104,11 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
-                    onChange={(e) => handleToggleTaskCompletion(e.target.checked, task.id)}
+                    onClick={() => handleToggleTaskCompletion(task.id)}
                   />
                   <span className="checkmark"></span>
                 </label>
@@ -97,11 +116,11 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={(e) => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
